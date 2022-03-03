2420688f9bd92cbf1a283bca6484811df51bd427
ljk1256 <ljk1256@naver.com>
2022-03-03 22:32:51 +0900

    ps day 2022/03/03 solve 2

--- Solution/BJ6236.java
+++ Solution/BJ6236.java
@@ -6,21 +6,52 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class BJ6236 {
+	
+	static int[] spendMoney;
+	static int spendCnt;
+	static int withdrawCnt;
+	
+	private static boolean binarySearch(int cost) { // 이분탐색시 매개변수가 필요한지 필요하다면 어떤 것을 기준으로 잡고 상관관계를 만들지 부터 생각하자
+		int tempCnt = 1; // 돈을 인출하고 시작해야 하니까
+		int tempMoney = cost;
+		
+		for(int i=0; i<spendCnt; i++) {
+			
+			if(tempMoney < spendMoney[i]) {
+				tempCnt++;
+				tempMoney = cost;
+				
+				if(tempMoney < spendMoney[i]) return false; // 애초에 인출된 금액으로 사용할 수 없는 금액이라면 더 이상 볼필요 없다
+			}
+			tempMoney -= spendMoney[i];
+		}
+		
+		if(tempCnt <= withdrawCnt) return true;
+		else return false;
+	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
+		spendCnt = Integer.parseInt(st.nextToken());
+		withdrawCnt = Integer.parseInt(st.nextToken());
 		
-		int maxMoney = 0;
+		spendMoney = new int [spendCnt];
+		int minSpend = 0, maxSpend = 1000000000; // 만약 최소 인출은 단 한번의 인출만 있을 경우다 한번에 최대금액
 		
-		for(int i=0; i<N; i++) {
-			maxMoney = Math.max(maxMoney, Integer.parseInt(br.readLine()));
+		for(int i=0; i<spendCnt; i++) {
+			spendMoney[i] = Integer.parseInt(br.readLine());
 		}
 		
-		System.out.println(maxMoney);
+		while(minSpend <= maxSpend) { // 배열의 인덱스 일 경우는 등호를 안넣지만 가격이나 어떤 수치적인 것들은 등호를 넣어주는 경우를 고려한다(인덱스 중복값은 등호 고려해줘야함)
+			int mid = minSpend + (maxSpend - minSpend) / 2;
+			
+			if(binarySearch(mid)) maxSpend = mid - 1;
+			else minSpend = mid + 1;
+		}
+		
+		System.out.println(minSpend);
 	}
 
 }
---
+++ Solution/BJ7795.java
@@ -0,0 +1,70 @@
+package Solution;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BJ7795 {
+	
+	static int[] B;
+	static int bSize;
+	
+	private static int binarySearch(int number) {
+		int left = 0, right = bSize-1;
+		
+		while(left <= right) {
+			int mid = left + (right - left) / 2;
+			
+			if(number < B[mid]) right = mid;
+			else left = mid + 1;
+		}
+		
+		// if(number == B[right]) return right - 1; // 자기 보다 작은 생물만 먹을 수 있음
+		return right;
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder builder = new StringBuilder();
+		
+		int TC = Integer.parseInt(br.readLine());
+		
+		for(int i=0; i<TC; i++) {
+			st = new StringTokenizer(br.readLine());
+			int aSize = Integer.parseInt(st.nextToken());
+			bSize = Integer.parseInt(st.nextToken());
+			
+			int[] A = new int [aSize]; // A 입력
+			
+			st = new StringTokenizer(br.readLine());
+			for(int j=0; j<aSize; j++) {
+				A[j] = Integer.parseInt(st.nextToken());
+			}
+			
+			B = new int [bSize]; // B 입력
+			
+			st = new StringTokenizer(br.readLine());
+			for(int j=0; j<bSize; j++) {
+				B[j] = Integer.parseInt(st.nextToken());
+			}
+			
+			int answer = 0;
+			
+			Arrays.sort(A);
+			Arrays.sort(B);
+			
+			for(int j=0; j<aSize; j++) {
+				answer += binarySearch(A[j]);
+			}
+			
+			builder.append(answer).append("\n");
+		}
+		
+		builder.setLength(builder.length()-1);
+		System.out.println(builder.toString());
+	}
+
+}
