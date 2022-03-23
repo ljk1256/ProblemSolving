81eb60c24e10145d2f8cfc56f81fdb4b5789e025
ljk1256 <ljk1256@naver.com>
2022-03-23 20:45:43 +0900

    ps day 2022/03/23 solve 1

---
+++ Solution/BJ3046.java
@@ -0,0 +1,20 @@
+package Solution;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ3046 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int R1 = Integer.parseInt(st.nextToken());
+		int S = Integer.parseInt(st.nextToken());
+		
+		System.out.println(2*S-R1);
+	}
+
+}
