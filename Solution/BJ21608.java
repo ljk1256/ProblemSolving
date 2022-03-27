26aef57613f9e3059829972403dd09af4f838a16
ljk1256 <ljk1256@naver.com>
2022-03-27 22:35:33 +0900

    ps day 2022/03/27 solve 1

---
+++ Solution/BJ21608.java
@@ -0,0 +1,23 @@
+package Solution;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ21608 {
+	
+	static class Candidate {
+		int x;
+		int y;
+		int cnt;
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+	}
+
+}
