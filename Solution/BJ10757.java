bf9a1a164bef84ebbfefa045be037bcca137cca5
ljk1256 <ljk1256@naver.com>
2022-04-02 23:06:40 +0900

    ps day 2022/04/02 solve 1

---
+++ Solution/BJ10757.java
@@ -0,0 +1,23 @@
+package Solution;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class BJ10757 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		BigInteger A = new BigInteger(st.nextToken());
+		BigInteger B = new BigInteger(st.nextToken());
+		
+		A = A.add(B);
+		
+		System.out.println(A);
+	}
+
+}
