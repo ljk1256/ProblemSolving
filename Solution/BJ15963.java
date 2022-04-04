65e4cffd412e28d9586683f5b0c3acf01f979f8b
ljk1256 <ljk1256@naver.com>
2022-04-04 19:59:05 +0900

    ps day 2022/04/04 solve 1

---
+++ Solution/BJ15963.java
@@ -0,0 +1,21 @@
+package Solution;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ15963 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		long N = Long.parseLong(st.nextToken());
+		long M = Long.parseLong(st.nextToken());
+		
+		if(N == M) System.out.println(1);
+		else System.out.println(0);
+	}
+
+}
