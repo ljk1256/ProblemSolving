668bd672183cfc052ad15a9fbcb9eb47536f6066
ljk1256 <ljk1256@naver.com>
2022-04-06 17:09:31 +0900

    ps day 2022/04/06 solve 1

---
+++ Solution/BJ2743.java
@@ -0,0 +1,15 @@
+package Solution;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ2743 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println(br.readLine().length());
+	}
+
+}
