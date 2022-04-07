569b8876827a0042dbdc4bdfbaa03a5fbb5f3958
ljk1256 <ljk1256@naver.com>
2022-04-07 18:26:36 +0900

    ps day 2022/04/07 solve 1

---
+++ Solution/BJ1550.java
@@ -0,0 +1,17 @@
+package Solution;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ1550 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine(), 16);
+		
+		System.out.println(N);
+	}
+
+}
